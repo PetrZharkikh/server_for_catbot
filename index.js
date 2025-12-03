efc02eef046d879bc47437b20af836807e2e0680
PetrZharkikh <142933608+PetrZharkikh@users.noreply.github.com>
2025-12-03 20:34:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -32,4 +32,5 @@ app.post("/webhook", async (req, res) => {
 
 app.get("/", (req, res) => res.send("CatBot server works!"));
 
-app.listen(3000, () => console.log("Server started!"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
